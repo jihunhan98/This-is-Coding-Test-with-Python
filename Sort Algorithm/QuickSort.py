4dda79b0a37c6e984d32905e9f819992b80a59b2
jihunhan98 <wlgnsdl12334@gmail.com>
2023-01-13 19:07:46 +0900

    DP Study Done

--- Sort Algorithm/QuickSort.py
+++ Sort Algorithm/QuickSort.py
@@ -4,7 +4,7 @@ array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
 def quick_sort(array):
     # 리스트가 하나 이하의 원소만을 담고 있다면 종료
     if len(array) <= 1:
-        return array
+        return array    
     pivot = array[0]  # 피벗은 첫 번째 원소
     tail = array[1:]  # 피벗을 제외한 리스트
 
